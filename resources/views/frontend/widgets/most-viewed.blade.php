de710bdc0eed28697ae9f749cece8836f24a89c0
01281326 <dervis.toprak@partner.turktelekom.com.tr>
2021-10-07 13:58:56 +0300

    updates

---
+++ resources/views/frontend/widgets/most-viewed.blade.php
@@ -0,0 +1,14 @@
+@if(isset($most_viewed))
+    <h4 class="text-info text-center">En Çok Okunanlar</h4>
+    @foreach($most_viewed as $most)
+<div class="list-group col-md-8">
+    <a href="{{route('single', [$most->category->slug, $most->slug])}}" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{$most->title}}</h5>
+            <small class="text-muted">{{$most->category->name}}</small>
+        </div>
+        <small class="text-muted">{{$most->created_at->diffForHumans()}}</small>
+    </a>
+</div>
+    @endforeach
+@endif
