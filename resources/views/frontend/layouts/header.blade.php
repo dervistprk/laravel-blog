91200286b5c0029f1655afd5859735832aef3862
dervistprk <dervis.toprak@partner.turktelekom.com.tr>
2021-10-13 17:31:55 +0300

    Some visual improvements General fixes

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - {{$config->title}}</title>
     <link rel="icon" type="image/x-icon" href="{{ URL::asset($config->favicon) }}"/>
     <!-- Font Awesome icons (free version)-->
